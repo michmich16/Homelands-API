26c525149d795d091c7efb51d672dd7d965d00b9
Michael P <102651473+michmich16@users.noreply.github.com>
2025-02-10 10:21:12 +0100

    images

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // import http from 'http';
 import express from 'express';
 import dotenv from 'dotenv';
+import  sequelize  from './Config/sequelizeClient.js';
 
 
 const app = express();
@@ -16,6 +17,17 @@ app.get('/about', (req, res) => {
 app.get('/contact', (req, res) => {
   res.send('Dette er kontakt siden...');
 });
+
+app.get('/test', async (req, res) => {
+    try {
+      await sequelize.authenticate();
+      console.log('Der er forbindelse til databasen');
+      res.send('Der er forbindelse til databasen');
+    } catch (error) {
+      console.error('Fejl! Kunne ikke forbinde til databasen: ', error);
+      res.status(500).send('Fejl! Kunne ikke forbinde til databasen.');
+    }
+  });
 
 app.listen(4242, () => {
   console.log("Express server kører....");
