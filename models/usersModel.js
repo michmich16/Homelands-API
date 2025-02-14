37299c0945cf8f47227de49d8dc7debc8e51143c
Michael P <102651473+michmich16@users.noreply.github.com>
2025-02-14 13:22:31 +0100

    done

--- models/usersModel.js
+++ models/usersModel.js
@@ -1,5 +1,6 @@
 import sequelize from "../config/sequelizeClient.js";
-import { Model, DataTypes } from 'sequelize'
+import { Model, DataTypes } from 'sequelize';
+import bcrypt from 'bcrypt';
 
 export class usersModel extends Model { }
 
@@ -21,25 +22,51 @@ usersModel.init({
     email: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: true, // Added unique constraint for email
+        validate: {
+            isEmail: true, // Validate that the value is a valid email
+        },
     },
     password: {
         type: DataTypes.STRING,
         allowNull: false,
-        //husk kryptering
+        // Remember to hash passwords before saving
     },
     refresh_token: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true, // Tokens can initially be null
     },
     is_active: {
         type: DataTypes.BOOLEAN,
         allowNull: false,
+        defaultValue: true, // Default to active
     },
 }, {
     sequelize,
     modelName: 'user',
-    underscored: true, // True: city_names || False: cityNames
-    freezeTableName: true, // True: city || False: cities
-    createdAt: true, // Tilføjer createdAt felt
-    updatedAt: true, // Tilføjer updatedAt felt
-})
+    underscored: true, // True: snake_case column names
+    freezeTableName: true, // Prevent Sequelize from pluralizing the table name
+    hooks: {
+        // Hook to hash password before creating a user
+        beforeCreate: async (user) => {
+            user.password = await createHash(user.password);
+        },
+        // Hook to hash password if it is changed before updating a user
+        beforeUpdate: async (user) => {
+            if (user.changed("password")) {
+                user.password = await createHash(user.password);
+            }
+        },
+    },
+    timestamps: true, // Adds createdAt and updatedAt fields
+});
+
+// Function to hash a string
+const createHash = async (string) => {
+    try {
+        const salt = await bcrypt.genSalt(10); // Generate a salt
+        return await bcrypt.hash(string, salt); // Hash the string with the salt
+    } catch (error) {
+        throw new Error("Error hashing password");
+    }
+};
